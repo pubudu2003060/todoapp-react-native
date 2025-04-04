8ca08ea80f5f7789b1b1da8d55d06fd8d8dd8018
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-04-04 23:19:02 +0530

    make simple add functions

--- App.tsx
+++ App.tsx
@@ -1,122 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View, Text, TextInput, SafeAreaView, StyleSheet, Button } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App() {
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const [task, setTask] = React.useState({
+    title: '',
+    completed: false,
+    description: '',
+    id: 0,
+  });
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  const [taskList, setTaskList] = React.useState([{}]);
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const addData = (key: string, value: string) => {
+    setTask(t => ({ ...t, [key]: value }));
+  }
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  const addTask = () => {
+    const newList = [...taskList, task];
+    setTaskList(newList);
+    setTask({
+      title: '',
+      completed: false,
+      description: '',
+      id: 0,
+    })
+  }
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-        contentContainerStyle={{padding: safePadding}}>
-        <Header />
-        <Section title="Step One">
-          Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-          screen and then come back to see your edits.
-        </Section>
-        <Section title="See Your Changes">
-          <ReloadInstructions />
-        </Section>
-        <Section title="Debug">
-          <DebugInstructions />
-        </Section>
-        <Section title="Learn More">
-          Read the docs to discover what to do next:
-        </Section>
-        <LearnMoreLinks />
-      </ScrollView>
-      
-    </View>
+    <SafeAreaView>
+      <View >
+        <Text style={styles.appTitle}> Todo App </Text>
+        <TextInput
+          value={task.title}
+          placeholder='Title...'
+          onChangeText={(text) => { addData("title", text) }}></TextInput>
+        <TextInput
+          value={task.description}
+          placeholder='Description...'
+          onChangeText={(text) => { addData("description", text) }}></TextInput>
+      </View>
+      <Button
+        title='add'
+        onPress={addTask}
+        accessibilityLabel='add task to list'></Button>
+
+      {taskList.map((t,index) => 
+        <Text>{t.title+" - "+t.description}</Text>
+      )}
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
+  appTitle: {
     fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#000',
+    textAlign: 'center',
+    marginTop: 50,
+  }
+})
+
 
 export default App;
